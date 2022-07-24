d280dc32204611e365b3d5369de445bff99cd403
anon-kae <anon.ksl42@gmail.com>
2022-07-24 19:09:48 +0700

    refactor: change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s',
-    title: 'image-uploader',
+    title: 'my-unsplash',
     htmlAttrs: {
       lang: 'en',
     },
